44c4f25d08789ea547042f14dc6ef233a8c5d359
Danya Kulbatsky <elited013@gmail.com>
2024-03-27 13:54:58 +0300

    created docs

--- src/main.ts
+++ src/main.ts
@@ -1,5 +1,6 @@
 import { NestFactory } from '@nestjs/core';
 import { AppModule } from './app.module';
+import { SwaggerModule, DocumentBuilder } from '@nestjs/swagger';
 import * as express from 'express';
 
 async function bootstrap() {
@@ -7,6 +8,14 @@ async function bootstrap() {
   app.setGlobalPrefix('api');
   app.enableCors();
   app.use(express.json({ limit: '10mb' }));
+  const config = new DocumentBuilder()
+    .setTitle('YooHive API')
+    .setDescription('The yoohive API description')
+    .setVersion('1.0')
+    .addTag('API')
+    .build();
+  const document = SwaggerModule.createDocument(app, config);
+  SwaggerModule.setup('api/docs', app, document);
   await app.listen(3000);
 }
 bootstrap();
--- src/user/dto/create-user.dto.ts
+++ src/user/dto/create-user.dto.ts
@@ -1,13 +1,18 @@
 import { IsEmail, MinLength } from 'class-validator';
+import { ApiProperty } from '@nestjs/swagger';
 
 export class CreateUserDto {
   @IsEmail()
+  @ApiProperty()
   email: string;
 
   @MinLength(6, { message: 'Password then be more then 6 smbls' })
+  @ApiProperty()
   password: string;
 
+  @ApiProperty()
   phone: string;
 
+  @ApiProperty()
   role: string;
 }
