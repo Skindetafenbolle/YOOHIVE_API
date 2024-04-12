b166e500aab26b79ac3731c6809f037c543156b3
Danya Kulbatsky <elited013@gmail.com>
2024-04-12 13:26:24 +0300

    check rebase

--- src/auth/roles.guard.ts
+++ src/auth/roles.guard.ts
@@ -22,6 +22,7 @@ export class RolesGuard implements CanActivate {
 
     const request = context.switchToHttp().getRequest();
     const user = request.user;
+    console.log(user);
 
     const companyIdFromRoute = +request.params.companyId;
     if (isNaN(companyIdFromRoute)) {
